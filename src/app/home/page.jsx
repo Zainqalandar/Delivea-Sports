8d495d67037da25b21029b71f5add0d3fb04f9c5
Zainqalandar <zainqlandar@gmail.com>
2024-12-02 07:08:52 +0500

    vercel problem issues commented

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,9 +1,9 @@
 import AboutUs from '@/components/about-us';
 import Banner from '@/components/Banner';
 import CenterBanner from '@/components/center-banner';
-import HeroSection from '@/components/hero-section';
+// import HeroSection from '@/components/hero-section';
 import Navbar from '@/components/Navbar';
-import MultipleItems from '@/components/new-products';
+// import MultipleItems from '@/components/new-products';
 import OurLocation from '@/components/our-location';
 import WhyUs from '@/components/why-us';
 import React from 'react';
@@ -13,8 +13,8 @@ const DelvieaSports = () => {
 		<>
 			<Banner />
             <Navbar />
-            <HeroSection />
-            <MultipleItems />
+            {/* <HeroSection />
+            <MultipleItems /> */}
 			<CenterBanner />
 			<AboutUs />
 			<WhyUs />
