391a6d14e32e0d98050ad53fd231d5cb72139a04
Zainqalandar <zainqlandar@gmail.com>
2024-12-04 20:36:49 +0500

    Slider added at hero section

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,8 +1,7 @@
 import AboutUs from '@/components/about-us';
 import Banner from '@/components/Banner';
-import CenterBanner from '@/components/center-banner';
 import Footer from '@/components/footer';
-// import HeroSection from '@/components/hero-section';
+import HeroSection from '@/components/hero-section';
 import Navbar from '@/components/Navbar';
 // import MultipleItems from '@/components/new-products';
 import OurLocation from '@/components/our-location';
@@ -13,11 +12,11 @@ import React from 'react';
 const DelvieaSports = () => {
 	return (
 		<>
+
 			<Banner />
             <Navbar />
-            {/* <HeroSection />
-            <MultipleItems /> */}
-			<CenterBanner />
+            <HeroSection />
+            {/* <MultipleItems /> */}
 			<OurServices />
 			<AboutUs />
 			<WhyUs />
--- src/components/hero-section/index.jsx
+++ src/components/hero-section/index.jsx
@@ -1,62 +1,268 @@
 'use client';
-import React from 'react';
-// import Slider from 'react-slick';
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import React, { useEffect } from 'react';
+import Image from 'next/image';
 
 const HeroSection = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 2000, // 2 seconds
-    };
+    useEffect(() => {
+        // Initialize the carousel functionality
+        const items = document.querySelectorAll('[data-carousel-item]');
+        let currentIndex = 0;
+
+        const showItem = (index) => {
+            items.forEach((item, i) => {
+                item.classList.toggle('block', i === index);
+                item.classList.toggle('hidden', i !== index);
+            });
+        };
+
+        const nextItem = () => {
+            currentIndex = (currentIndex + 1) % items.length;
+            showItem(currentIndex);
+        };
+
+        const prevItem = () => {
+            currentIndex = (currentIndex - 1 + items.length) % items.length;
+            showItem(currentIndex);
+        };
+
+        document.querySelector('[data-carousel-next]').addEventListener('click', nextItem);
+        document.querySelector('[data-carousel-prev]').addEventListener('click', prevItem);
+
+        showItem(currentIndex);
+
+        const interval = setInterval(nextItem, 2000);
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <>
-        {/* <div className="slider-container relative w-full">
-            <Slider {...settings}>
-                <div className="relative h-56 md:h-96">
-                    <img
-                        src="/banner-images/t5.jpg"
-                        className="absolute block w-full h-full object-cover"
-                        alt="Slide 1"
-                    />
-                    <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white p-2">
-                        <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-                            <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-                                Better Data
-                            </span>{' '}
-                            Scalable AI.
-                        </h1>
+            <div
+                id="default-carousel"
+                className="relative w-full"
+                data-carousel="slide"
+            >
+                {/* Carousel wrapper */}
+                <div className="relative h-72 md:h-96 lg:h-128 bg-gray-200 flex items-center justify-center overflow-hidden rounded-lg">
+                    {/* Item 1 */}
+                    <div
+                        className="block duration-700 ease-in-out"
+                        data-carousel-item=""
+                    >
+                        <Image
+                            src="/banner-images/t3.jpg"
+                            alt="Banner"
+                            layout="fill"
+                            objectFit="cover"
+                            className="absolute inset-0"
+                        />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                            <div className="text-center text-white p-4">
+                                <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold mb-4">
+                                    Welcome to Our Website
+                                </h1>
+                                <p className="text-lg md:text-xl lg:text-2xl">
+                                    We provide the best services to help you grow your business.
+                                </p>
+                                <button className="mt-4 px-6 py-2 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded">
+                                    Learn More
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Item 2 */}
+                    <div
+                        className="hidden duration-700 ease-in-out"
+                        data-carousel-item=""
+                    >
+                        <Image
+                            src="/gloves/g1.jpg"
+                            alt="Banner"
+                            layout="fill"
+                            objectFit="cover"
+                            className="absolute inset-0"
+                        />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                            <div className="text-center text-white p-4">
+                                <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold mb-4">
+                                    Welcome to Our Website
+                                </h1>
+                                <p className="text-lg md:text-xl lg:text-2xl">
+                                    We provide the best services to help you grow your business.
+                                </p>
+                                <button className="mt-4 px-6 py-2 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded">
+                                    Learn More
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Item 3 */}
+                    <div
+                        className="hidden duration-700 ease-in-out"
+                        data-carousel-item=""
+                    >
+                        <Image
+                            src="/gloves/g2.jpg"
+                            alt="Banner"
+                            layout="fill"
+                            objectFit="cover"
+                            className="absolute inset-0"
+                        />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                            <div className="text-center text-white p-4">
+                                <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold mb-4">
+                                    Welcome to Our Website
+                                </h1>
+                                <p className="text-lg md:text-xl lg:text-2xl">
+                                    We provide the best services to help you grow your business.
+                                </p>
+                                <button className="mt-4 px-6 py-2 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded">
+                                    Learn More
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Item 4 */}
+                    <div
+                        className="hidden duration-700 ease-in-out"
+                        data-carousel-item=""
+                    >
+                        <Image
+                            src="/banner-images/t3.jpg"
+                            alt="Banner"
+                            layout="fill"
+                            objectFit="cover"
+                            className="absolute inset-0"
+                        />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                            <div className="text-center text-white p-4">
+                                <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold mb-4">
+                                    Welcome to Our Website
+                                </h1>
+                                <p className="text-lg md:text-xl lg:text-2xl">
+                                    We provide the best services to help you grow your business.
+                                </p>
+                                <button className="mt-4 px-6 py-2 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded">
+                                    Learn More
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Item 5 */}
+                    <div
+                        className="hidden duration-700 ease-in-out"
+                        data-carousel-item=""
+                    >
+                        <Image
+                            src="/gloves/g3.jpg"
+                            alt="Banner"
+                            layout="fill"
+                            objectFit="cover"
+                            className="absolute inset-0"
+                        />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                            <div className="text-center text-white p-4">
+                                <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold mb-4">
+                                    Welcome to Our Website
+                                </h1>
+                                <p className="text-lg md:text-xl lg:text-2xl">
+                                    We provide the best services to help you grow your business.
+                                </p>
+                                <button className="mt-4 px-6 py-2 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded">
+                                    Learn More
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="relative h-56 md:h-96">
-                    <img
-                        src="/banner-images/t1.jpg"
-                        className="absolute block w-full h-full object-cover"
-                        alt="Slide 2"
+                {/* Slider indicators */}
+                <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+                    <button
+                        type="button"
+                        className="w-3 h-3 rounded-full"
+                        aria-current="true"
+                        aria-label="Slide 1"
+                        data-carousel-slide-to={0}
                     />
-                    <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white p-2">
-                    <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Get back to growth with <span className="text-blue-600 dark:text-blue-500">the world's #1</span> CRM.</h1>
-                    </div>
-                </div>
-                <div className="relative h-56 md:h-96">
-                    <img
-                        src="/banner-images/t2.jpg"
-                        className="absolute block w-full h-full object-cover"
-                        alt="Slide 3"
+                    <button
+                        type="button"
+                        className="w-3 h-3 rounded-full"
+                        aria-current="false"
+                        aria-label="Slide 2"
+                        data-carousel-slide-to={1}
                     />
-                    <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white p-2">
-                    <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Regain <mark className="px-2 text-white bg-blue-600 rounded dark:bg-blue-500">control</mark> over your days</h1>
-                    </div>
+                    <button
+                        type="button"
+                        className="w-3 h-3 rounded-full"
+                        aria-current="false"
+                        aria-label="Slide 3"
+                        data-carousel-slide-to={2}
+                    />
+                    <button
+                        type="button"
+                        className="w-3 h-3 rounded-full"
+                        aria-current="false"
+                        aria-label="Slide 4"
+                        data-carousel-slide-to={3}
+                    />
+                    <button
+                        type="button"
+                        className="w-3 h-3 rounded-full"
+                        aria-current="false"
+                        aria-label="Slide 5"
+                        data-carousel-slide-to={4}
+                    />
                 </div>
-            </Slider>
-        </div> */}
+                {/* Slider controls */}
+                <button
+                    type="button"
+                    className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                    data-carousel-prev=""
+                >
+                    <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                        <svg
+                            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 6 10"
+                        >
+                            <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M5 1 1 5l4 4"
+                            />
+                        </svg>
+                        <span className="sr-only">Previous</span>
+                    </span>
+                </button>
+                <button
+                    type="button"
+                    className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                    data-carousel-next=""
+                >
+                    <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                        <svg
+                            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 6 10"
+                        >
+                            <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="m1 9 4-4-4-4"
+                            />
+                        </svg>
+                        <span className="sr-only">Next</span>
+                    </span>
+                </button>
+            </div>
         </>
     );
 };
